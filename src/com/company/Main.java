2f453e7407ffba2901bbeb0147d5b5420bd36197
macbookair <nj.martin123@gmail.com>
2015-10-26 15:15:34 -0400

    homework 4.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList <Post> posts = new ArrayList<>();
+        Spark.staticFileLocation("/public");
+        Spark.init();
+        Spark.post(
+                "/create-user",
+                ((request, response) -> {
+                    User user = new User();
+                    user.name = request.queryParams("name");
+                    users.add(user);
+                    response.redirect("/posts");
+                    return "";
+                })
+        );
+
+        Spark.post(
+                "/create-post",
+                ((request, response) -> {
+                    Post post = new Post();
+                    post.text = request.queryParams("post");
+                    posts.add(post);
+                    response.redirect("/posts");
+                    return "";
+                })
+        );
+
+        Spark.get(
+                "/posts",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    m.put("name", users.get(0).name);
+                    m.put("posts", posts);
+                    return new ModelAndView(m, "posts.html");
+                },
+                new MustacheTemplateEngine()
+                );
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by macbookair on 10/26/15.
+ */
+public class User {
+     String name;
+}
