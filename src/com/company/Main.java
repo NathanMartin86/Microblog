ba401f76fcb6d1f429104d29d7c0632fa73b2f81
macbookair <nj.martin123@gmail.com>
2015-10-27 15:19:41 -0400

    homework 4.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,40 @@
 package com.company;
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList<>();
-        ArrayList <Post> posts = new ArrayList<>();
+        ArrayList<Post> posts = new ArrayList<>();
         Spark.staticFileLocation("/public");
         Spark.init();
+
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("name");
+                    if (username == null) {
+                        return new ModelAndView(new HashMap<>(), "index.html");//Why does modelandview require a HashMap?
+                    }
+                    HashMap m = new HashMap();
+                    m.put("name",username);
+                    m.put("posts", posts);
+                    return new ModelAndView(m, "posts.html");
+                },
+                new MustacheTemplateEngine()
+        );
         Spark.post(
                 "/create-user",
                 ((request, response) -> {
-                    User user = new User();
-                    user.name = request.queryParams("name");
-                    users.add(user);
-                    response.redirect("/posts");
+                    String username = request.queryParams("name");
+                    Session session= request.session();
+                    session.attribute("name",username);
+                    response.redirect("/");
                     return "";
                 })
         );
@@ -28,22 +43,48 @@ public class Main {
                 "/create-post",
                 ((request, response) -> {
                     Post post = new Post();
+                    post.id = posts.size() + 1;
                     post.text = request.queryParams("post");
                     posts.add(post);
-                    response.redirect("/posts");
+                    response.redirect("/");
                     return "";
                 })
         );
 
-        Spark.get(
-                "/posts",
-                (request, response) -> {
-                    HashMap m = new HashMap();
-                    m.put("name", users.get(0).name);
-                    m.put("posts", posts);
-                    return new ModelAndView(m, "posts.html");
-                },
-                new MustacheTemplateEngine()
-                );
+
+
+
+        Spark.post( //Why aren't we creating a session here? When's this necessary?
+                "/delete-post",
+                ((request, response) -> {
+                    String id = request.queryParams("postid");
+                    try {
+                        int idNum = Integer.valueOf(id);
+                        posts.remove(idNum - 1);
+                        for (int i = 0; i < posts.size(); i++) {
+                            posts.get(i).id = i + 1;
+                        }
+                    } catch (Exception e) {
+                    }
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/edit-post",
+                ((request, response) -> {
+                    String id = request.queryParams("postid");
+                    try {
+                        int idNum = Integer.valueOf(id);
+                        Post post = posts.get(idNum-1);
+                        post.text = request.queryParams("editpost");
+                    }catch(Exception e){
+
+                    }
+                    response.redirect("/");
+                    return "";
+                })
+        );
     }
 }
+
--- src/com/company/Passwords.java
+++ src/com/company/Passwords.java
@@ -3,7 +3,7 @@ package com.company;
 /**
  * Created by macbookair on 10/26/15.
  */
-public class Post {
-    int id;
-    String text;
+public class Passwords {
+    String password;
+
 }
